ace4fc6e89152b77de286079972e0ba0705727ac
superying <slsuperrabbit@gmail.com>
2020-02-25 13:44:33 -0500

    add models

---
+++ lib/core/loss.py
@@ -0,0 +1,104 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+
+
+class JointsMSELoss(nn.Module):
+    def __init__(self, use_target_weight):
+        super(JointsMSELoss, self).__init__()
+        self.criterion = nn.MSELoss(reduction='mean')
+        self.use_target_weight = use_target_weight
+
+    def forward(self, output, target, target_weight):
+        batch_size = output.size(0)
+        num_joints = output.size(1)
+        heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
+        heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
+        loss = 0
+
+        # print('num_joints: ', num_joints)
+        # print('heatmaps_pred type: ', type(heatmaps_pred))
+        # print('heatmaps_gt type: ', type(heatmaps_gt))
+        # print('target_weight type: ', type(target_weight))
+        # print('heatmaps_pred size: ', len(heatmaps_pred))
+        # print('heatmaps_gt size: ', len(heatmaps_gt))
+        # print('target_weight size: ', target_weight.size())
+
+        for idx in range(num_joints):
+            heatmap_pred = heatmaps_pred[idx].squeeze()
+            heatmap_gt = heatmaps_gt[idx].squeeze()
+
+            tmp1 = heatmap_pred.mul(target_weight[:, idx])
+            tmp2 = heatmap_gt.mul(target_weight[:, idx])
+
+
+            # print('heatmaps_pred size: ', len(heatmaps_pred[idx]))
+            # print('heatmaps_gt size: ', len(heatmaps_gt[idx]))
+            # print('tmp1 size: ', len(tmp1))
+            # print('tmp2 size: ', len(tmp2))
+
+
+            if self.use_target_weight:
+                loss += 0.5 * self.criterion(tmp1, tmp2)
+                # loss += 0.5 * self.criterion(
+                #     heatmap_pred.mul(target_weight[:, idx]),
+                #     heatmap_gt.mul(target_weight[:, idx])
+                # )
+            else:
+                loss += 0.5 * self.criterion(heatmap_pred, heatmap_gt)
+
+        return loss / num_joints
+
+
+class JointsOHKMMSELoss(nn.Module):
+    def __init__(self, use_target_weight, topk=8):
+        super(JointsOHKMMSELoss, self).__init__()
+        self.criterion = nn.MSELoss(reduction='none')
+        self.use_target_weight = use_target_weight
+        self.topk = topk
+
+    def ohkm(self, loss):
+        ohkm_loss = 0.
+        for i in range(loss.size()[0]):
+            sub_loss = loss[i]
+            topk_val, topk_idx = torch.topk(
+                sub_loss, k=self.topk, dim=0, sorted=False
+            )
+            tmp_loss = torch.gather(sub_loss, 0, topk_idx)
+            ohkm_loss += torch.sum(tmp_loss) / self.topk
+        ohkm_loss /= loss.size()[0]
+        return ohkm_loss
+
+    def forward(self, output, target, target_weight):
+        batch_size = output.size(0)
+        num_joints = output.size(1)
+        heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
+        heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
+
+        loss = []
+        for idx in range(num_joints):
+            heatmap_pred = heatmaps_pred[idx].squeeze()
+            heatmap_gt = heatmaps_gt[idx].squeeze()
+            if self.use_target_weight:
+                loss.append(0.5 * self.criterion(
+                    heatmap_pred.mul(target_weight[:, idx]),
+                    heatmap_gt.mul(target_weight[:, idx])
+                ))
+            else:
+                loss.append(
+                    0.5 * self.criterion(heatmap_pred, heatmap_gt)
+                )
+
+        loss = [l.mean(dim=1).unsqueeze(dim=1) for l in loss]
+        loss = torch.cat(loss, dim=1)
+
+        return self.ohkm(loss)
---
+++ lib/models/__init__.py
@@ -0,0 +1,22 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.pose_resnet
+import models.pose_hrnet
+
+import models.pose_hrnetv2
+
+import models.abNetv21
+import models.abNetv42
+import models.abNetv48
